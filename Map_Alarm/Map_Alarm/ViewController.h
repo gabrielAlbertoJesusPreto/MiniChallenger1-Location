84cb7655042b84b3277a254a05e22b2a7cdc4ee6
Lidia Chou <lidia.chou94@gmail.com>
2015-03-02 08:52:02 -0300

    Projeto inicial

---
+++ Map_Alarm/Map_Alarm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Map_Alarm
+//
+//  Created by Lidia Chou on 3/2/15.
+//  Copyright (c) 2015 Lidia Chou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Map_Alarm/Map_Alarm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Map_Alarm
+//
+//  Created by Lidia Chou on 3/2/15.
+//  Copyright (c) 2015 Lidia Chou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
