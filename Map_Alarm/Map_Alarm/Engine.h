1e0c60d20f683ec1a184985fc15b36152d9d63be
Evandro Viva <vivaerp@gmail.com>
2015-03-06 18:18:31 -0300

    Criado Class Engine

---
+++ Map_Alarm/Map_Alarm/Engine.h
@@ -0,0 +1,13 @@
+//
+//  Engine.h
+//  Map_Alarm
+//
+//  Created by Evandro Remon Pulz Viva on 3/6/15.
+//  Copyright (c) 2015 Lidia Chou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Engine : NSObject
+
+@end
---
+++ Map_Alarm/Map_Alarm/Engine.m
@@ -0,0 +1,13 @@
+//
+//  Engine.m
+//  Map_Alarm
+//
+//  Created by Evandro Remon Pulz Viva on 3/6/15.
+//  Copyright (c) 2015 Lidia Chou. All rights reserved.
+//
+
+#import "Engine.h"
+
+@implementation Engine
+
+@end
